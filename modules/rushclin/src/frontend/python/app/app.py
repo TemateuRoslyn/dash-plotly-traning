3200ed6e22fd06f27a3e9732a87aea13fac9685c
rushclin02 <takamrushclin@gmail.com>
2023-03-19 14:14:44 +0100

    Add About section

--- modules/rushclin/src/frontend/python/app/app.py
+++ modules/rushclin/src/frontend/python/app/app.py
@@ -1,9 +1,11 @@
 import os
 from dash import Dash, html, dcc, Input, Output
+import numpy as np
 import plotly.express as px
 import pandas as pd
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
+import plotly.figure_factory as ff
 
 debug = False if os.environ["DASH_DEBUG_MODE"] == "False" else True
 
@@ -15,6 +17,7 @@ app.title='Finances'
 apple_df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
 prices_df = px.data.stocks()
 gapminder_df =  px.data.gapminder()
+
 # Style du sidebar
 SIDEBAR_STYLE = {
     "position": 'fixed', 
@@ -81,6 +84,7 @@ def render_page_content(pathname):
     if(pathname == '/'): 
         return [
             html.H1("Le marché financier de Apple", className='text-center'),
+            html.Hr(),
             dcc.Graph(
                 id='apple_graph', 
                 figure=go.Figure(
@@ -124,10 +128,34 @@ def render_page_content(pathname):
         return [
             html.H1('A propos de moi', className='text-center'), 
             html.Hr(),
+            html.Blockquote(
+                children=[
+                    html.P("Crée dans le cadre de l'apprentissage de Dash - Plotly"), 
+                    html.Figcaption("Rushclin Takam", className='blockquote-footer'),
+                ], 
+                className='blockquote'
+            ), 
+            html.Ul(
+                children=[
+                    html.Li("Prise en main de Dash", className='list-item'),
+                    html.Li("Installation de dash_bootstrap_components"),
+                    html.Li("Mise en place d'un Layout"),
+                    html.Li("Ajout des liens  hypertexts dans l'application"),
+                    html.Li("Changement de l'apparence de Bootstrap en choisissant son thème"),
+                    html.Li("Creation des graphes avec dash_core_component"),
+                    html.Li("Utilisation minimale de Docker"),
+                    html.Li("Utilisation des callback pour donner du dynamisme à l'application"),
+                ], 
+                className='list'
+            )
         ]
     elif(pathname == '/histogram'): 
         return [
-            
+            html.H1('Graphe avec des histogrammes', className='text-center'), 
+            html.Hr(),
+            dcc.Graph(
+                id='histogram', 
+            )
         ]
     # Au cas ou l'utilisateur aie entré une url invalide, on lui renvois la page 404
     return dbc.Alert("Oups... La page n'a pas été trouvée. 😴️", color='danger', className='text-center')
@@ -149,7 +177,7 @@ def render_prices_graph(marche):
 def render_graph_1(year): 
     filtered_df = gapminder_df[gapminder_df.year == year]
     fig = px.histogram(filtered_df, x='lifeExp', color='continent')
-    return fig
+    return fig  
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8050", debug=debug)
