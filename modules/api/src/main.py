e82914a9977ca1d8d27efb142b4c9f655409ec0c
Temateu Roslyn <temateuroslynf32@gmail.com>
2023-04-23 13:06:15 +0100

    deuxieme route

--- modules/api/src/main.py
+++ modules/api/src/main.py
@@ -7,11 +7,13 @@ from controllers.accelerometre1_controller import *
 
 # models
 from models.accelerometre1 import Accelerometre1
+from models.accelerometre2 import Accelerometre2
 
 
 app = Flask(__name__)
 
 acc1 =  Accelerometre1()
+acc2 =  Accelerometre2()
 
 
 @app.route('/', methods=['GET'])
@@ -19,8 +21,13 @@ def index():
     return "Welcome to this example api for dash plottly"
 
 @app.route('/accelerometre1/next', methods=['GET'])
-def get_employees():
+def get_accelerometre1():
     nextCapteurValue = acc1.get_next()
+    return json.dumps(nextCapteurValue)
+
+@app.route('/accelerometre2/next', methods=['GET'])
+def get_accelerometre2():
+    nextCapteurValue = acc2.get_next()
     return json.dumps(nextCapteurValue)
 
 
