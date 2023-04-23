35dd88046fa7031850a72b628557fd124a3d14ea
Temateu Roslyn <temateuroslynf32@gmail.com>
2023-04-23 13:14:08 +0100

    eajout des autrs groupe poiur le  doeesnes

--- modules/api/src/main.py
+++ modules/api/src/main.py
@@ -6,14 +6,21 @@ import json
 from controllers.accelerometre1_controller import *
 
 # models
+
 from models.accelerometre1 import Accelerometre1
 from models.accelerometre2 import Accelerometre2
+
+from models.comparateur1 import Comparateur1
+from models.comparateur2 import Comparateur2
 
 
 app = Flask(__name__)
 
 acc1 =  Accelerometre1()
 acc2 =  Accelerometre2()
+
+comp1 = Comparateur1()
+comp2 = Comparateur2()
 
 
 @app.route('/', methods=['GET'])
@@ -29,6 +36,17 @@ def get_accelerometre1():
 def get_accelerometre2():
     nextCapteurValue = acc2.get_next()
     return json.dumps(nextCapteurValue)
+
+@app.route('/comparateur1/next', methods=['GET'])
+def get_comparateur1():
+    nextCapteurValue = comp1.get_next()
+    return json.dumps(nextCapteurValue)
+
+@app.route('/comparateur2/next', methods=['GET'])
+def get_comparateur2():
+    nextCapteurValue = comp2.get_next()
+    return json.dumps(nextCapteurValue)
+
 
 
 if __name__ == '__main__':
