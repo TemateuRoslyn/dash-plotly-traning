c4859dc448be5d64caf87821630454e806b12599
Merritz <morritzngueguim43@gmail.com>
2023-05-14 08:25:44 -0400

    update docker file

--- modules/morritz/redis/start-app.sh
+++ modules/morritz/redis/start-app.sh
@@ -3,5 +3,5 @@ cd ./app
 docker build --target dev . -t python
 docker run -it -v ${PWD}:/work python sh
 
-/work # python --version
-Python 3.9.
+# /work # python --version
+# Python 3.9.
--- modules/morritz/redis/start-redis-store.sh
+++ modules/morritz/redis/start-redis-store.sh
@@ -6,20 +6,19 @@ docker network create redis
 cd ./storage/
 
 #redis-0
-docker run -d --rm --name redis-0 `
-    --net redis `
-    -v ${PWD}/redis-0:/etc/redis/ `
-    redis redis-server /etc/redis/redis.conf
+docker run -d --rm --name redis-0 --net redis -v ${PWD}/redis-0:/etc/redis/ redis redis-server /etc/redis/redis.conf
 
 #redis-1
-docker run -d --rm --name redis-1 `
-    --net redis `
-    -v ${PWD}/redis-1:/etc/redis/ `
-    redis redis-server /etc/redis/redis.conf
-
+docker run -d --rm --name redis-1 --net redis -v ${PWD}/redis-1:/etc/redis/ redis redis-server /etc/redis/redis.conf
 
 #redis-2
-docker run -d --rm --name redis-2 `
-    --net redis `
-    -v ${PWD}/redis-2:/etc/redis/ `
-    redis redis-server /etc/redis/redis.conf
+docker run -d --rm --name redis-2 --net redis -v ${PWD}/redis-2:/etc/redis/ redis redis-server /etc/redis/redis.conf
+
+# sentinel-0
+docker run -d --rm --name sentinel-0 --net redis -v ${PWD}/sentinel-0:/etc/redis/ redis redis-sentinel /etc/redis/sentinel.conf
+
+# sentinel-1
+docker run -d --rm --name sentinel-1 --net redis -v ${PWD}/sentinel-1:/etc/redis/ redis redis-sentinel /etc/redis/sentinel.conf
+
+# sentinel-2
+docker run -d --rm --name sentinel-2 --net redis -v ${PWD}/sentinel-2:/etc/redis/ redis redis-sentinel /etc/redis/sentinel.conf
